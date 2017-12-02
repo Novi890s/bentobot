144ff97963c8a0ad985b001dc3ba893c55ce3669
codingcosmonaut <32783057+codingcosmonaut@users.noreply.github.com>
2017-12-02 15:04:24 -0500

    fix dependencies

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ require('dotenv').config();
 const { CommandoClient, SQLiteProvider  } = require('discord.js-commando');
 const path = require('path');
 const sqlite = require('sqlite');
-const Enmap = require('enmap');
-const EnmapLevel = require("enmap-level");
+
 //Init SQLite settings provider.
 sqlite.open(path.join(__dirname, "settings.sqlite3")).then((db) => {
     client.setProvider(new SQLiteProvider(db));
-});
-//Open points DB
-const pointProvider = new EnmapLevel({name: "points"});
-this.points = new Enmap({provider: pointProvider});
+};
+
 //Initialize client
 const client = new CommandoClient({
     commandPrefix: '[bento]',
