d01a0d75db35d6c65ca4a96840960f83734f834d
codingcosmonaut <32783057+codingcosmonaut@users.noreply.github.com>
2017-12-02 18:21:13 -0500

    fix httpcat command

--- commands/utility/http.cat.js
+++ commands/utility/http.cat.js
@@ -8,20 +8,21 @@ module.exports = class HttpCatCommand extends Command {
             name: 'httpcat',
             group: 'utility',
             memberName: 'httpcat',
-            description: 'HTTPcat api',
+            description: 'HTTPcats',
             examples: ['httpcat code'],
             userPermissions: ['SEND_MESSAGES'],
             args: [
                 {
-                    key: 'code',
+                    key: 'text',
                     prompt: 'What is the HTTP Code?',
-                    type: 'string'
+                    type: 'integer'
                 }
             ]
         });
     }
-    run(msg, {text}) {
-        //Put code in
-        return msg.say("http://http.cat/" + text);
+    run(msg, { text }) {
+        msg.delete();
+        //Put HTTP code in URL
+        return msg.say('http://http.cat/' +  text );
     }
 };
