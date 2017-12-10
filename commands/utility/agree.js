4c92b546be38f55682859b6be45d42cc41313e35
codingcosmonaut <32783057+codingcosmonaut@users.noreply.github.com>
2017-12-10 17:03:50 -0500

    Add new style with Prettier

--- commands/utility/agree.js
+++ commands/utility/agree.js
@@ -1,26 +1,26 @@
 //Import Commando.
-const { Command } = require('discord.js-commando');
+const { Command } = require("discord.js-commando");
 //Initialize command.
 module.exports = class ReplyCommand extends Command {
-    constructor(client) {
-        super(client, {
-            //Define attributes of the command.
-            name: 'agree',
-            group: 'utility',
-            memberName: 'agree',
-            description: 'Agree to the rules.',
-            examples: ['agree'],
-            userPermissions: ['SEND_MESSAGES']
-        });
-    }
-    run(msg) {
-        //Find role in server named 'Member'
-        var memberRole = msg.guild.roles.find('name', 'Member');
-        //Add role and log it.
-        msg.member.addRole(memberRole, "Agreed to rules");
-        //Delete command
-        msg.delete();
-        //Confirm verification
-        return msg.say("You agreed to the rules. Happy chatting!");
-    }
+  constructor(client) {
+    super(client, {
+      //Define attributes of the command.
+      name: "agree",
+      group: "utility",
+      memberName: "agree",
+      description: "Agree to the rules.",
+      examples: ["agree"],
+      userPermissions: ["SEND_MESSAGES"]
+    });
+  }
+  run(msg) {
+    //Find role in server named 'Member'
+    var memberRole = msg.guild.roles.find("name", "Member");
+    //Add role and log it.
+    msg.member.addRole(memberRole, "Agreed to rules");
+    //Delete command
+    msg.delete();
+    //Confirm verification
+    return msg.say("You agreed to the rules. Happy chatting!");
+  }
 };
--- commands/utility/http.cat.js
+++ commands/utility/http.cat.js
@@ -1,28 +1,28 @@
 //Import Commando.
-const { Command } = require('discord.js-commando');
+const { Command } = require("discord.js-commando");
 //Initialize command.
 module.exports = class HttpCatCommand extends Command {
-    constructor(client) {
-        super(client, {
-            //Define attributes of the command.
-            name: 'httpcat',
-            group: 'utility',
-            memberName: 'httpcat',
-            description: 'HTTPcats',
-            examples: ['httpcat code'],
-            userPermissions: ['SEND_MESSAGES'],
-            args: [
-                {
-                    key: 'text',
-                    prompt: 'What is the HTTP Code?',
-                    type: 'integer'
-                }
-            ]
-        });
-    }
-    run(msg, { text }) {
-        msg.delete();
-        //Put HTTP code in URL
-        return msg.say('http://http.cat/' +  text );
-    }
+  constructor(client) {
+    super(client, {
+      //Define attributes of the command.
+      name: "httpcat",
+      group: "utility",
+      memberName: "httpcat",
+      description: "HTTPcats",
+      examples: ["httpcat code"],
+      userPermissions: ["SEND_MESSAGES"],
+      args: [
+        {
+          key: "text",
+          prompt: "What is the HTTP Code?",
+          type: "integer"
+        }
+      ]
+    });
+  }
+  run(msg, { text }) {
+    msg.delete();
+    //Put HTTP status code in URL
+    return msg.say("http://http.cat/" + text);
+  }
 };
