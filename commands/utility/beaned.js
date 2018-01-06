5d8f5105a99494e0adb82263555455dceb32efb8
codingcosmonaut <suushii@users.noreply.github.com>
2018-01-06 15:13:49 -0500

    add you just got beaned

---
+++ commands/utility/beaned.js
@@ -0,0 +1,30 @@
+//Import Commando.
+const { Command } = require("discord.js-commando");
+//Initialize command.
+module.exports = class ReplyCommand extends Command {
+  constructor(client) {
+    super(client, {
+      //Define attributes of the command.
+      name: "beaned",
+      group: "utility",
+      memberName: "beaned",
+      description: "Get beaned",
+      examples: ["beaned @user"],
+      userPermissions: ["SEND_MESSAGES"],
+      args: [
+        {
+          key: "text",
+          prompt: "Who do you want to bean?",
+          type: "user"
+        }
+      ]
+    });
+  }
+  run(msg, { text }) {
+    msg.delete();
+    //you just got beaned
+    return msg.channel.send(
+      text + " http://i2.kym-cdn.com/photos/images/newsfeed/001/166/608/7d5.jpg"
+    );
+  }
+};
