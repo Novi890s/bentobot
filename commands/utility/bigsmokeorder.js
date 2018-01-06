df498c409c02eddbbd22ac2560eeadb9017eef01
codingcosmonaut <suushii@users.noreply.github.com>
2018-01-06 14:18:11 -0500

    Added Big Smoke's order

---
+++ commands/utility/bigsmokeorder.js
@@ -0,0 +1,24 @@
+//Import Commando.
+const { Command } = require("discord.js-commando");
+//Initialize command.
+module.exports = class ReplyCommand extends Command {
+  constructor(client) {
+    super(client, {
+      //Define attributes of the command.
+      name: "bigsmokeorder",
+      group: "utility",
+      memberName: "bigsmokeorder",
+      description: "Big Smoke's order",
+      examples: ["bigsokeorder"],
+      userPermissions: ["SEND_MESSAGES"]
+    });
+  }
+  run(msg) {
+    //Delete command
+    msg.delete();
+    //Big Smoke's Order
+    return msg.say(
+      "I’ll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda."
+    );
+  }
+};
