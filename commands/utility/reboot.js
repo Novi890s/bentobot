5e8599d9b3d7d75186545e5e73cc9ace5cf36bc2
codingcosmonaut <suushii@users.noreply.github.com>
2018-01-13 13:41:16 -0500

    Add rebooting message

--- commands/utility/reboot.js
+++ commands/utility/reboot.js
@@ -1,26 +1,25 @@
 //Import Commando.
-const {
-    Command
-} = require('discord.js-commando');
+const { Command } = require("discord.js-commando");
 //Initialize command.
 module.exports = class ReplyCommand extends Command {
-    constructor(client) {
-        super(client, {
-            //Define attributes of the command.
-            name: 'reboot',
-            group: 'utility',
-            memberName: 'reboot',
-            description: 'Reboot all instances of the bot.',
-            examples: ['agree']
-        });
-    }
-    hasPermission(msg) {
-        return this.client.isOwner(msg.author);
-    }
+  constructor(client) {
+    super(client, {
+      //Define attributes of the command.
+      name: "reboot",
+      group: "utility",
+      memberName: "reboot",
+      description: "Reboot all instances of the bot.",
+      examples: ["agree"]
+    });
+  }
+  hasPermission(msg) {
+    return this.client.isOwner(msg.author);
+  }
 
-    run(msg) {
-        msg.delete();
-        process.exit(0);
-        return;
-    }
+  run(msg) {
+    msg.delete();
+    client.user.setGame("Restarting bot...");
+    process.exit(0);
+    return;
+  }
 };
