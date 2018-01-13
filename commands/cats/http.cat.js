995116ea3779087a699dc0649889cd9dcafbbc05
codingcosmonaut <suushii@users.noreply.github.com>
2018-01-13 14:26:56 -0500

    Improve httpcat with message embeds

--- commands/cats/http.cat.js
+++ commands/cats/http.cat.js
@@ -22,7 +22,34 @@ module.exports = class HttpCatCommand extends Command {
   }
   run(msg, { text }) {
     msg.delete();
-    //Put HTTP status code in URL
-    return msg.say("http://http.cat/" + text);
-  }
+    let embed = new Discord.RichEmbed()
+      .setTitle("It's Caturday!")
+      .setAuthor("Bento", "https://i.imgur.com/lm8s41J.png")
+      /*
+       * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+       */
+      .setColor(0x00ae86)
+      .setImage(
+        "http.cat/" + text
+      )
+      /*
+       * Takes a Date object, defaults to current date.
+       */
+      .setTimestamp();
+
+    msg.channel.send({ embed });
+    embed = new Discord.RichEmbed()
+      .setTitle("It's Caturday!")
+      .setAuthor("Bento", "https://i.imgur.com/lm8s41J.png")
+      /*
+       * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+       */
+      .setColor(0x00ae86)
+      .setImage(
+        "http.cat/" + text
+      )
+      /*
+       * Takes a Date object, defaults to current date.
+       */
+      .setTimestamp();
 };
