4546dba89969e3a55de4d535a1f2fbfe24bef8d7
suushii <32783057+suushii@users.noreply.github.com>
2018-01-15 09:33:53 -0500

    Add info command

---
+++ commands/info/info.js
@@ -0,0 +1,24 @@
+//Import Commando.
+const { Command } = require("discord.js-commando");
+//Initialize command.
+module.exports = class ReplyCommand extends Command {
+  constructor(client) {
+    super(client, {
+      //Define attributes of the command.
+      name: "info",
+      group: "info",
+      memberName: "info",
+      description: "info",
+      examples: ["info"],
+      userPermissions: ["SEND_MESSAGES"]
+    });
+  }
+  run(msg) {
+    //Delete command
+    msg.delete();
+    //Return info
+    return msg.channel.send(
+      "BentoBot is maintained by suushii. Visit suushii.github.io/bentobot."
+    );
+  }
+};
