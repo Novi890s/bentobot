4f0762b49092b531ae496d790d280db44b6fc366
suushii <32783057+suushii@users.noreply.github.com>
2018-03-06 18:31:36 -0500

    idiot-proof musicbot

---
+++ commands/utility/musichelp.js
@@ -0,0 +1,18 @@
+const { Command } = require("discord.js-commando");
+
+module.exports = class SayCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "musichelp",
+      group: "utility",
+      memberName: "musichelp",
+      description: "Get music help",
+      examples: ["musichelp"]
+    });
+  }
+
+  run(msg, {}) {
+    msg.delete();
+    return msg.channel.send("Do >musichelp to get started!");
+  }
+};
